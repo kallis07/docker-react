33c331b02e3d75109062f9e776e9ded7bdcec5df
root <root@ip-172-31-30-5.us-east-2.compute.internal>
2020-03-25 12:14:50 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello KALYAN,Welcome to the page.!!! How are you doing today.
         </p>
         <a
           className="App-link"
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
@@ -7,3 +8,13 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+
